7fdde03902b13fd067c00dffc1e4a0a525eebfd8
GHOST <s68122250060@ssru.ac.th>
2026-07-05 12:55:54 +0700

    Delete src directory

--- src/RoleUser.java
+++
@@ -1,5 +0,0 @@
-public enum RoleUser {
-    PublicRelation,
-    Division,
-    Manager
-}
--- src/StatusProposal.java
+++
@@ -1,9 +0,0 @@
-public enum StatusProposal {
-    Waiting,
-    Rejected,
-    Accepted;
-
-    public void changeStatus() {
-        // change status logic
-    }
-}
